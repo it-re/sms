6b2967316f2cfc1694ec370e12dd2010bb21ab13
yu-pon <ymg2465079@stu.o-hara.ac.jp>
2025-06-17 12:29:03 +0900

    岸_全体50%完了

---
+++ src/scoremanager/main/SubjectCreateExecuteAction.java
@@ -0,0 +1,71 @@
+package scoremanager.main;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bean.School;
+import bean.Subject;
+import bean.Teacher;
+import dao.SubjectDao;
+import tool.Action;
+
+public class SubjectCreateExecuteAction extends Action {
+
+	@Override
+	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+
+		// ローカル変数の指定
+		HttpSession session = req.getSession(); // セッション
+		Teacher teacher = (Teacher)session.getAttribute("user");
+		String subject_cd = ""; //科目コード
+		String subject_name = ""; //科目名
+		Subject subject = new Subject();
+		School school = new School();
+		SubjectDao subjectDao = new SubjectDao();
+		Map<String, String> errors = new HashMap<>(); // エラーメッセージ
+
+		//リクエストパラメーターの取得
+		subject = subjectDao.get(subject_cd, school);
+		subject_cd = req.getParameter("cd");
+		subject_name = req.getParameter("name");
+
+		if (subject_cd.length() == 3) {
+			errors.put("1", "科目コードは3文字で入力してください。");
+			// リクエストにエラーメッセージをセット
+			req.setAttribute("errors", errors);
+		} else {
+
+			if (subject != null) { // 科目コードが重複している場合
+				errors.put("2", "科目コードが重複しています");
+				// リクエストにエラーメッセージをセット
+				req.setAttribute("errors", errors);
+			} else {
+				// subjectに学生情報をセット
+				subject.setCd(subject_cd);
+				subject.setName(subject_name);
+				subject.setSchool(teacher.getSchool());
+				// saveメソッドで情報を登録
+				subjectDao.save(subject);
+			}
+		}
+
+		//リクエストに値をセット
+		req.setAttribute("cd", subject_cd);
+		req.setAttribute("name", subject_name);
+
+
+		// JSPへフォワード
+		if (errors.isEmpty()) { // エラーメッセージがない場合
+			// 登録完了画面にフォワード
+			req.getRequestDispatcher("student_create_done.jsp").forward(req, res);
+		} else { // エラーメッセージがある場合
+			// 登録画面にフォワード
+			req.getRequestDispatcher("StudentCreate.action").forward(req, res);
+		}
+	}
+
+}
