f3a008679a338d1adeff1f4ece9ed40d8db1c611
Yua025 <ymg2465009@stu.o-hara.ac.jp>
2025-06-20 12:19:11 +0900

    半田 科目削除(サーブレット実装)完成

--- src/scoremanager/main/SubjectDeleteAction.java
+++ src/scoremanager/main/SubjectDeleteAction.java
@@ -4,33 +4,35 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import bean.School;
+import bean.Subject;
 import bean.Teacher;
 import dao.SubjectDao;
 import tool.Action;
 
 public class SubjectDeleteAction extends Action {
 
-	@Override
-	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+    @Override
+    public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
 
-		/* セッションからユーザーデータを取得 */
-		HttpSession session = req.getSession();
-		Teacher teacher = (Teacher)session.getAttribute("user");
+        // セッションから教員情報を取得
+        HttpSession session = req.getSession();
+        Teacher teacher = (Teacher) session.getAttribute("user");
 
-		/* 必要なローカル変数の定義 */
-		SubjectDao subjectDao = new SubjectDao();
+        // リクエストから科目コードを取得
+        String cd = req.getParameter("cd");
 
-		/* 科目コードを取得する */
-		String cd = req.getParameter("subject_cd");
+        // 学校情報を取得
+        School school = teacher.getSchool();
 
-		/* 科目名を取得する */
-		String name = req.getParameter("subject_name");
+        // DAOで科目情報を取得
+        SubjectDao subjectDao = new SubjectDao();
+        Subject subject = subjectDao.get(cd, school);
 
-		/* 削除処理 */
-		subjectDao.delete(cd, teacher.getSchool());
+        // JSPに科目情報をセット
+        req.setAttribute("subject", subject);
 
-		// 削除完了ページへフォワード
-	    req.getRequestDispatcher("subject_delete_done.jsp").forward(req, res);
-	}
-
+        // 削除確認画面へフォワード
+        req.getRequestDispatcher("subject_delete.jsp").forward(req, res);
+    }
 }
---
+++ src/scoremanager/main/SubjectDeleteExecuteAction.java
@@ -0,0 +1,61 @@
+package scoremanager.main;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bean.Subject;
+import bean.Teacher;
+import dao.SubjectDao;
+import tool.Action;
+
+public class SubjectDeleteExecuteAction extends Action {
+
+	@Override
+	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+
+		/* セッションからユーザーデータを取得 */
+		HttpSession session = req.getSession();
+		Teacher teacher = (Teacher)session.getAttribute("user");
+
+		// ローカル変数の指定
+		String subject_cd = ""; //科目コード
+		String subject_name = ""; //科目名
+		Subject subject = new Subject();
+		SubjectDao subjectDao = new SubjectDao();
+		Map<String, String> errors = new HashMap<>(); // エラーメッセージ
+
+		//リクエストパラメーターの取得
+		subject_cd = req.getParameter("subject_cd");
+		subject_name = req.getParameter("subject_name");
+
+		if (subjectDao.get(subject_cd, teacher.getSchool())== null) { // 科目コードが重複している場合
+			errors.put("1", "科目が存在していません");
+			// リクエストにエラーメッセージをセット
+			req.setAttribute("errors", errors);
+		} else {
+			// subjectに科目情報をセット
+			subject.setCd(subject_cd);
+			subject.setName(subject_name);
+			subject.setSchool(teacher.getSchool());
+			// saveメソッドで情報を登録
+			subjectDao.delete(subject);
+		}
+
+		//リクエストに値をセット
+		req.setAttribute("cd", subject_cd);
+		req.setAttribute("name", subject_name);
+
+		// JSPへフォワード
+		if (errors.isEmpty()) { // エラーメッセージがない場合
+			// 登録完了画面にフォワード
+			req.getRequestDispatcher("subject_delete_done.jsp").forward(req, res);
+		} else { // エラーメッセージがある場合
+			// 登録画面にフォワード
+			req.getRequestDispatcher("SubjectDelete.action").forward(req, res);
+		}
+	}
+}
