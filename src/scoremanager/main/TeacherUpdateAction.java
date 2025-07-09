21a473e3702fb709cb09073f5ec1d21b650dac82
2465084 <ymg2465084@stu.o-hara.ac.jp>
2025-07-09 10:40:40 +0900

    syusei-sagara

--- src/scoremanager/main/TeacherUpdateAction.java
+++ src/scoremanager/main/TeacherUpdateAction.java
@@ -17,33 +17,38 @@ public class TeacherUpdateAction extends Action {
 		HttpSession session = req.getSession(); // セッション
 		Teacher teacher = (Teacher)session.getAttribute("user");
 
-		// ローカル変数の指定 1
+		/**ローカル変数の指定**/
 		String id = ""; // 教師ID
-		String name= ""; // 氏名
-		String password = "";//パスワード
+		String name= ""; // 教師名
 		boolean isAdmin = false; // 管理者権限フラグ
-		Teacher teacherData = new Teacher();
-		TeacherDao teacherDao = new TeacherDao();
+		Teacher teacherData = new Teacher();//bean_Teacher
+		TeacherDao teacherDao = new TeacherDao();//TeacherDao
 
-		// リクエストパラメーターの取得 2
+		// リクエストパラメーターの取得
 		id = req.getParameter("id");
-		
-		
+
+
 		// DBからデータ取得
 		teacherData = teacherDao.get(id);
-		
+		/** 現在ログインしている教師のIDをString型で取得 **/
+		String loginId = teacher.getId();
+
+		/** beanのteacherDataから取得 **/
 		name = teacherData.getName();
 		isAdmin = teacherData.isAdmin();
 
-		// レスポンス値をセット
-		// リクエストに学生番号をセット
+		/**レスポンス値をセット**/
+		/**リクエストに教師IDをセット**/
 		req.setAttribute("id", id);
-		// リクエストに氏名をセット
+		/** リクエストに教師名をセット**/
 		req.setAttribute("name", name);
-		// リクエストに在学フラグをセット
+		/**リクエストに管理者権限をセット**/
 		req.setAttribute("is_admin", isAdmin);
+		/** ログインしている教師のIDをセット **/
+		req.setAttribute("LoginId", loginId);
 
-		// JSPへフォワード 7
+
+		// JSPへフォワード
 		req.getRequestDispatcher("teacher_update.jsp")
 			.forward(req, res);
 	}
